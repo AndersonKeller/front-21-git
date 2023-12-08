ee4504bd32da9275c5dc1629ff45912544faae24
Anderson Keller <andersonkellervendas@hotmail.com>
2023-12-08 19:20:03 -0300

    craida a tela de login

--- api.js
+++
@@ -1,2 +0,0 @@
-export const baseUrl = "http://localhost:3001"
-// export const baseUrl = "https://server-senac-json-andersonkeller2.onrender.com"
--- script.js
+++
@@ -1,8 +0,0 @@
-import {baseUrl} from "./api.js"
-async function login(){
-
-    const res = await fetch(`${baseUrl}/login`,{method:"POST"})
-    const resJson = await res.json()
-    console.log(resJson)
-}
-login()
